46321a06dd764fda781269c446a80fb0cf0376f6
akinsam23 <akinsam23@gmail.com>
2023-06-20 15:05:00 -0700

    updat

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -21,8 +21,8 @@ int print_sign(int n)
     }
     else if (n < 0)
     {
-        _putchar('-');
-        return (-1);
+	    _putchar('-');
+	    return (-1);
     }
 
     /*Default case if none of the conditionals are met*/
