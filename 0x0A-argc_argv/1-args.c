457a6e021241e8ce1ecc9bab71a8f14d0ff9f9e5
akinsam23 <akinsam23@gmail.com>
2023-07-27 04:45:17 -0700

    updated name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,6 +2,7 @@
 #include "main.h"
 
 /*
+ * main - print the number of arguments passed to the program
  *Write a program that prints the number of arguments passed into it.
  *Your program should print a number, followed by a new line.
  *@argc: mnumber of arguments.
@@ -11,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv,
+	(void) argv, /*Ignore argv*/
 	printf("%d\n", argc - 1);
 	return (0);
 }
